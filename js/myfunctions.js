af05e23624567e1dc96ede3286c206ec13c56c9b
Logan McAnsh <l.m@loganmcansh.com>
2015-08-16 13:45:29 -0400

    transparent nav is back
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -2,3 +2,12 @@ $("#toggle").click(function(){
   $("nav").toggleClass("is-open");
   $("nav").toggleCSS('display', 'none');
 });
+
+$(window).scroll (function () {
+  var sT = $(this).scrollTop();
+    if (sT >= 650) {
+        $('nav').addClass('scroll');
+    } else {
+        $('nav').removeClass('scroll');
+    }
+});
