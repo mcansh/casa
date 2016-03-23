486f399fd41f82489ae49a8d397f79ee07a28fc4
Logan McAnsh <l.m@loganmcansh.com>
2016-03-23 16:27:41 -0400

    went wayyyy to long without pushing, main changes are in the javascript and nav layout
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- assets/js/min/myfunction-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){var l=$("nav");$(window).scroll(function(){var s=$(window).scrollTop();s>=200?l.addClass("scroll"):l.removeClass("scroll")})});var $hamburger=$(".hamburger"),$links=$(".links");$hamburger.on("click",function(){$hamburger.toggleClass("is-active"),$links.toggleClass("is-open"),$links.toggleClass("blur")});
--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -1,22 +1,28 @@
-$(function() {
-  var nav = $("nav");
-  $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
+var $nav = $("nav");
+// blur the nav
+// manually becuase of a webkit bug when using classes
+if ($(window).width() > 700) {
+  $(function() {
+    $(window).scroll(function() {
+        var $scroll = $(window).scrollTop();
 
-      if (scroll >= 200) {
-          nav.addClass('scroll');
-      } else {
-          nav.removeClass("scroll");
-      }
+        if ($scroll >= 200) {
+          $nav.css({"background-color": "rgba(107, 194, 129, 0.5)", "-webkit-backdrop-filter": "blur(10px)"});
+          $nav.css("height", "5em");
+        } else {
+          $nav.css({"background-color": "rgba(107, 194, 129, 0.0)", "-webkit-backdrop-filter": "blur(0px)"});
+          $nav.css("height", "7em");
+        }
+    });
   });
-});
+}
 
-
+// Mobile Nav
 var $hamburger = $(".hamburger");
 var $links = $(".links");
 $hamburger.on("click", function() {
   $hamburger.toggleClass("is-active");
   // Do something else, like open/close menu
+  $nav.toggleClass("is-open");
   $links.toggleClass("is-open");
-  $links.toggleClass("blur");
 });
