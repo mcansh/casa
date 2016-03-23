ce307ab6e03d2b73fabf3e62f747c6a6ff5b35f3
Logan McAnsh <l.m@loganmcansh.com>
2016-03-23 17:51:04 -0400

    just need the why section, macbook is at like 3%
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -1,3 +1,5 @@
+
+// would have all vars here but that breaks the function????!
 var $nav = $("nav");
 // blur the nav
 // manually becuase of a webkit bug when using classes
@@ -6,7 +8,7 @@ if ($(window).width() > 700) {
     $(window).scroll(function() {
         var $scroll = $(window).scrollTop();
 
-        if ($scroll >= 200) {
+        if ($scroll >= 100) {
           $nav.css({"background-color": "rgba(107, 194, 129, 0.5)", "-webkit-backdrop-filter": "blur(10px)"});
           $nav.css("height", "5em");
         } else {
